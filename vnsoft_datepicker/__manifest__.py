3d882d801bcaa32310ca9732fe9b4cea3de3ba94
vnsoft <vnsoft.he@gmail.com>
2017-11-14 11:07:23 +0800

    Add files via upload

---
+++ vnsoft_datepicker/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'vnsoft_datepicker',
+    'version': '0.1',
+    'category': 'hide',
+    'sequence': 20,
+    'summary': 'Tools',
+    'description': """
+Add calendar selection plug-in function, you can limit the date selection range
+==========================================
+
+    """,
+    'author': 'VnSoft',
+    'website': 'https://www.odoo.com/page/crm',
+    # 'images': ['images/Sale_order_line_to_invoice.jpeg','images/sale_order.jpeg','images/sales_analysis.jpeg'],
+    'depends': ['web'],
+    'data': ["datepicker.xml"],
+    "qweb":[],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'price': 1.99,
+    'currency': 'EUR'
+}
